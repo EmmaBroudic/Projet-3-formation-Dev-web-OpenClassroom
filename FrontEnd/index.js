e41bd7c0838bdac8746c31de603e7341efe6315c
EmmaBroudic <emma.broudic35@gmail.com>
2023-02-24 18:51:23 +0100

    1.1 et 2.2 du P3

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -27,23 +27,114 @@ works.forEach(work => {
 });
 
 // Tri à partir des filtres
-// Faire trois boutons qui filtrent chacun pour une catégorie
+
 // bouton sélectionner tout
+const boutonTous = document.querySelector(".btn-tous");
 
+boutonTous.addEventListener("click", function () {
+    
+// Invisibiliser le contenu HTML de la gallerie travaux
+    gallery.innerHTML = "";
 
-// bouton sélectionner Objets
+// Afficher les travaux
+    works.forEach(work => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
 
-const reponseBoutonObjets = await fetch("http://localhost:5678/api/categories");
-const categories = await reponseBoutonObjets.json();
+        image.src = work.imageUrl;
+        figcaption.textContent = work.title;
 
-const boutonTrier = document.querySelector(".btn-trier-obj");
+        gallery.appendChild(figure);
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        });
+    });
 
-boutonTrier.addEventListener("click", function () {
-    const objets = categories.map(categorie => categorie.objets);
-    for(let i = categories.length -1 ; i >= 0; i--){
-    if(categories[i].name != "Objets"){
-        objets.splice(i,1)
-   }
-}
-console.log(objets)
+// bouton sélectionner "Objets"
+
+const boutonTrierObj = document.querySelector(".btn-trier-obj");
+
+boutonTrierObj.addEventListener("click", function () {
+    // récupérer les travaux correspondant à la catégorie "Objets"
+    const objets = works.filter(work => work.categoryId === 1);
+
+    console.log(works);
+    console.log(objets);
+
+    // supprimer les travaux précédemment affichés dans la galerie
+    gallery.innerHTML = "";
+
+    // afficher les travaux correspondant à la catégorie "Objets"
+    objets.forEach(objet => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+
+        image.src = objet.imageUrl;
+        figcaption.textContent = objet.title;
+
+        gallery.appendChild(figure);
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+    });
 });
+
+// bouton sélectionner Appartements
+
+const boutonTrierAppt = document.querySelector(".btn-trier-appt");
+
+boutonTrierAppt.addEventListener("click", function () {
+    // récupérer les travaux correspondant à la catégorie "Appartements"
+    const appartements = works.filter(work => work.categoryId === 2);
+
+    console.log(works);
+    console.log(appartements);
+
+    // supprimer les travaux précédemment affichés dans la galerie
+    gallery.innerHTML = "";
+
+    // afficher les travaux correspondant à la catégorie "Appartements"
+    appartements.forEach(appartement => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+
+        image.src = appartement.imageUrl;
+        figcaption.textContent = appartement.title;
+
+        gallery.appendChild(figure);
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+    });
+});
+
+// bouton sélectionner "Hôtels & restaurants"
+
+const boutonTrierHotels = document.querySelector(".btn-trier-hotel");
+
+boutonTrierHotels.addEventListener("click", function () {
+    // récupérer les travaux correspondant à la catégorie "Appartements"
+    const hotels = works.filter(work => work.categoryId === 3);
+
+    console.log(works);
+    console.log(hotels);
+
+    // supprimer les travaux précédemment affichés dans la galerie
+    gallery.innerHTML = "";
+
+    // afficher les travaux correspondant à la catégorie "Appartements"
+    hotels.forEach(hotel => {
+        const figure = document.createElement("figure");
+        const image = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+
+        image.src = hotel.imageUrl;
+        figcaption.textContent = hotel.title;
+
+        gallery.appendChild(figure);
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+    });
+});
+
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,6 +1,55 @@
-fetch("http://localhost:5678/api/users/login", {
-    method: 'POST',
-    body: JSON.stringify({email: email, password: passwords}),
-    headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
-}).then(//à coder//données à récupérer au format json)
-  .then(//à coder//)
+// déclarer les constantes formulaire et bouton, les lier au code html
+
+const formulaire = document.querySelector(".login-form");
+const submitButton = document.querySelector(".submit-btn");
+
+// activer le click à la soumission des données formulaires (email et mot de passe)
+formulaire.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    // récupérer les données email et mot de passe, les lier au code html
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+    // comparer les données utilisateur (email et mot de passe) aux données conservées dans l'API
+
+    // récupérer les données dans l'API - méthode POST
+    fetch("http://localhost:5678/api/users/login", {
+      method: 'POST',
+      body: JSON.stringify({ email: email, password: password }),
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+      })
+
+      // passer les données au format JSON
+      .then(response => response.json())
+
+      // comparer les données utilisateur
+      .then(data => {
+        
+        // tester sur la console la récupération des données
+        console.log(data);
+        console.log(email);
+        console.log(password);
+        
+        // si les données sont les bonnes, connexion réussie
+        if (data.token) {
+          alert("Connexion réussie !");
+          
+          // conserver les données dans le local storage
+          window.localStorage.setItem("token", data.token);
+          
+          // basculer vers la page d'accueil
+          window.location.href = "index.html";
+          
+          console.log(data.userId);
+          console.log(data.token);
+          
+          // Si les données utilisateurs sont incorrectes, lui indiquer par un message
+          } else {
+          alert("Identifiants incorrects.");
+
+          }
+      })
+      
+      .catch(error => console.error(error));
+});
